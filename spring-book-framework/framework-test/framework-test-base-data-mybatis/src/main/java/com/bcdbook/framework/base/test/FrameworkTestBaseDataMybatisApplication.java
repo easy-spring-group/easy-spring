f8f05b5c88dfaeedfb7714c3db602b8ce6ded405
summer <lixian13149999@163.com>
2018-12-02 20:22:44 +0800

    [开发] 加入测试类的结构

---
+++ spring-book-framework/framework-test/framework-test-base-data-mybatis/src/main/java/com/bcdbook/framework/base/test/FrameworkTestBaseDataMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.bcdbook.framework.base.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkTestBaseDataMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkTestBaseDataMybatisApplication.class, args);
+    }
+}
---
+++ spring-book-framework/framework-test/framework-test-base-data-mybatis/src/test/java/com/bcdbook/framework/base/test/FrameworkTestBaseDataMybatisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bcdbook.framework.base.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrameworkTestBaseDataMybatisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
